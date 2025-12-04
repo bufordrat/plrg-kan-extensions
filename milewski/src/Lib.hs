05a50868a19f9a922f3dd911902a900135b1dc8b
Matt Teichman <teichman@uchicago.edu>
2025-12-04 10:06:28 -0600

    milewski project

--- milewski/src/Lib.hs
+++ milewski/src/Lib.hs
@@ -1,4 +1,19 @@
 module Lib where
 
-msg :: String
-msg = "Hello, MPCS 51400 World!"
+import Data.Tree
+
+-- Milewski right Kan extension datatype
+newtype Ran k d a = Ran { unRan :: forall i. (a -> k i) -> d i }
+
+exampleRan :: a -> Ran Tree [] a
+exampleRan a = Ran (\f -> flatten (f a))
+
+intToTree :: Int -> Tree Int
+intToTree n =
+  let coreducer n | n <= 0 = (0, [])
+      coreducer n = (n, n - 1 : [n - 2])
+  in unfoldTree coreducer n
+
+printTree :: Show a => Tree a -> IO ()
+printTree = putStrLn . drawTree . fmap show
+
