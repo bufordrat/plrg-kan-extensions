2d7fa1f94d6f247ea699ff4495a96e514f641500
Matt Teichman <teichman@uchicago.edu>
2025-12-05 12:22:51 -0600

    add lan stuff

--- milewski/src/Lib.hs
+++ milewski/src/Lib.hs
@@ -1,6 +1,11 @@
+{-# LANGUAGE RankNTypes #-}
+
 module Lib where
 
 import Data.Tree
+
+printTree :: Show a => Tree a -> IO ()
+printTree = putStrLn . drawTree . fmap show
 
 -- Milewski right Kan extension datatype
 newtype Ran k d a = Ran { unRan :: forall i. (a -> k i) -> d i }
@@ -14,6 +19,10 @@ intToTree n =
       coreducer n = (n, n - 1 : [n - 2])
   in unfoldTree coreducer n
 
-printTree :: Show a => Tree a -> IO ()
-printTree = putStrLn . drawTree . fmap show
+-- Milewski left Kan extension datatype
+data Lan k d a = forall i. Lan (k i -> a) (d i)
 
+exampleLan :: Monoid a => a -> Lan [] Tree a
+exampleLan x = Lan mconcat (Node x [Node x [], Node x []])
+
+-- let (Lan f tr) = exampleLan 12 in f (flatten tr)
